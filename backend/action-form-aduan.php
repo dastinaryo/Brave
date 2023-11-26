006674b84e9ff6f3892291b6861efb24fef3b9bb
dastinaryo5@gmail.com <dastinaryo2@gmail.com>
2023-11-26 23:20:08 +0700

    test

--- backend/action-form-aduan.php
+++ backend/action-form-aduan.php
@@ -4,6 +4,7 @@
     
     // menangkap data yang di kirim dari form
     $nim = $_POST['user_id'];
+    $nama = $_POST['username'];
     $jdl = $_POST['jdl'];
     $tgl = $_POST['tgl'];    
     $plk = $_POST['plk'];
@@ -16,7 +17,7 @@
     $tmp_file = $_FILES['file']['tmp_name'];
 
     // Lokasi folder untuk menyimpan file yang diunggah
-    $folder_tujuan = "../assets/img/bukti/";
+    $folder_tujuan = "upload_files/";
 
     // Membuat path lengkap file tujuan
     $path_tujuan = $folder_tujuan . $nama_file;
@@ -36,8 +37,8 @@
     $id_pengaduan = $randomId;
 
     // menginput data ke database
-    $sql = "INSERT INTO pengaduan(id_pengaduan, nim, judul, tgl_kejadian, pesan_aduan, pelaku, bukti, status) values
-                        ('$id_pengaduan','$nim','$jdl', '$tgl', '$det', '$plk', '$nama_file', 'pending')";
+    $sql = "INSERT INTO pengaduan(id_pengaduan, nama, nim, judul, tgl_kejadian, pesan_aduan, pelaku, bukti, status) values
+                        ('$id_pengaduan', '$nama', '$nim','$jdl', '$tgl', '$det', '$plk', '$nama_file', 'pending')";
     
     // Memindahkan file ke folder tujuan
     if (move_uploaded_file($tmp_file, $path_tujuan)) {
